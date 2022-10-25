b344d1cef3226a82f34c99489204809b2fdf0eb2
Kawayan <kawayan.deguzman@gmail.com>
2022-10-24 23:46:38 -0700

    add constant BASE_URL with api for exchange rate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+const BASE_URL = 'https://api.exchangeratesapi.io/v1/latest'
+
 function App() {
   return (
     <>
