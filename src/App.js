0c5fe4ac3e3e3f4c280f9b0d6526bef45b8acf57
Kawayan <kawayan.deguzman@gmail.com>
2022-10-25 00:14:33 -0700

    add const setToCurrency and setFromCurrency

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ const BASE_URL = 'https://api.exchangerate.host/latest'
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
 
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
       })
   }, [])
 
