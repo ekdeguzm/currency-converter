1410af4ce8cc79507fd4cd97a805e07800ab334f
Kawayan <kawayan.deguzman@gmail.com>
2022-10-25 21:17:39 -0700

    remove duplicate keys error by removing data.base to allow 1 instance of EUR

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
   const [fromCurrency, setFromCurrency] = useState()
   const [toCurrency, setToCurrency] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
 
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
         const firstCurrency = Object.keys(data.rates)[0]
-        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setCurrencyOptions([...Object.keys(data.rates)])
         setFromCurrency(data.base)
         setToCurrency(firstCurrency)
       })
